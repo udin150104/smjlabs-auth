e843282ad5e651df6423a5fe0bfa7e58cbd54fd6
syahrudinsmj <syahrudinsmj@gmail.com>
2025-07-24 03:20:56 +0700

    update login

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -5,13 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME').' - Halaman Login' }}</title>
+    <title>{{ env('APP_NAME') . ' - Halaman Login' }}</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="@smjlabs_auth_assets('css/style.css')">
 </head>
 
-<body id="login-app" data-js="{{ $includejs ?? ''}}" data-base-url="/smjlabs-auth-assets/">
-    @yield('content')
+<body>
+    <div id="login-app" data-barba="wrapper">
+        <div id="content" data-barba="container" data-js="{{ $includejs ?? '' }}"
+            data-base-url="/smjlabs-auth-assets/" data-barba-namespace="login">
+            @yield('content')
+        </div>
+    </div>
     <script src="@smjlabs_auth_assets('js/app.js')" type="module"></script>
 </body>
 
